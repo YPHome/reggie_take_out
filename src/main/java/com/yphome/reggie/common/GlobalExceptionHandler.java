8dd3268463d5a22ef13ad4a188cf6063cbb9d950
YPHOME <linpingping190@gmail.com>
2022-11-21 19:45:38 +0800

    update some features

---
+++ src/main/java/com/yphome/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.yphome.reggie.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+
+/**
+ * 全局异常处理
+ */
+
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@ResponseBody
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+        return R.error("失败了");
+    }
+}
--- src/main/java/com/yphome/reggie/controller/EmployeeController.java
+++ src/main/java/com/yphome/reggie/controller/EmployeeController.java
@@ -10,6 +10,8 @@ import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RequestMapping("/employee")
@@ -52,6 +54,21 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request){
         request.getSession().removeAttribute("employee");
         return R.success("退出成功！");
+
+    }
+
+    @PostMapping
+    public R<String> save (HttpServletRequest request,@RequestBody Employee employee){
+        log.info("新增员工，员工信息{}", employee.toString());
+
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setCreateUser((Long)request.getSession().getAttribute("employee"));
+        employee.setUpdateUser((Long)request.getSession().getAttribute("employee"));
+
+        employeeService.save(employee);
+        return R.success("新增员工成功！");
 
     }
 
